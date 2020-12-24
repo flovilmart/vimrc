bf4abecbbaa014b864640708226cb3a4510825d3
Florent Vilmart <florent@flovilmart.com>
2020-12-24 17:03:50 -0500

    Fix install script

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 set -e
 
 cd ~/.vim_runtime
+if [ -f ~/.vimrc ];
+then
+  cat ~/.vimrc > ~/.vimrc.bak
+  echo "Backed up ~/.vimrc in ~/.vimrc.bak"
+  rm ~/.vimrc
+fi
 ln -s ~/.vim_runtime/vimrcs/vimrc ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
