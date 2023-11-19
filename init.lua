6de73d8fc18949b9c55915cbc65f2c2790578325
Florent Vilmart <florent@flovilmart.com>
2023-11-19 10:29:55 -0500

    cleanup vimRCs

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 vim.g.mapleader = ","
 
 require('flovilmart')
-
--- vim.cmd('source ~/.config/nvim/vimrcs/basic.vim')
--- lua/flovilmart/init.lua
+++ lua/flovilmart/init.lua
@@ -2,17 +2,14 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+  -- Style and coloring
   Plug ('altercation/vim-colors-solarized')
   Plug ('sheerun/vim-wombat-scheme')
-  Plug ('tpope/vim-vinegar')
-
   Plug ('vim-airline/vim-airline')
   Plug ('vim-airline/vim-airline-themes')
-  Plug ('github/copilot.vim')
 
   --" Searching
   Plug ( 'junegunn/fzf', { dir = '~/.fzf', ['do'] = vim.fn['fzf#install']})
-
   Plug ( 'junegunn/fzf.vim')
 
   --" TreeSitter: Code parsing
@@ -25,29 +22,34 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
   Plug ('airblade/vim-gitgutter')
 
   -- Language support
-  --" Plug ('sheerun/vim-polyglot')
   Plug ('dense-analysis/ale')
 
+  -- LSP support, completion and snippets
   Plug ('neovim/nvim-lspconfig')
+
+  Plug ('hrsh7th/nvim-cmp')
   Plug ('hrsh7th/cmp-nvim-lsp')
   Plug ('hrsh7th/cmp-buffer')
   Plug ('hrsh7th/cmp-path')
   Plug ('hrsh7th/cmp-cmdline')
-  Plug ('hrsh7th/nvim-cmp')
   Plug ('hrsh7th/vim-vsnip')
+
+  Plug ('github/copilot.vim')
+
+  -- Rust
   Plug ('simrat39/rust-tools.nvim')
 
   -- Java
   Plug ('mfussenegger/nvim-jdtls')
 
+  -- Rails
+  Plug ('tpope/vim-rails')
+
+  -- Navigation
   Plug ('nvim-lua/plenary.nvim')
   Plug ('nvim-telescope/telescope.nvim')
   Plug ('nvim-telescope/telescope-file-browser.nvim')
 
-  --. Plug ('vim-ruby/vim-ruby')
-  Plug ('tpope/vim-rails')
-
-  -- " Plug ('fatih/vim-go', { 'do': ':GoUpdateBinaries' }
 
   -- " Additional features
   Plug ('mg979/vim-visual-multi')
@@ -61,15 +63,6 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
     Plug ('jebaum/vim-tmuxify')
     Plug ('jgdavey/tslime.vim')
   end
-
-  Plug ('leafgarland/typescript-vim')
-
-  -- " Syntax coloring
-  -- " Plug ('ap/vim-css-color')
-  Plug ('cespare/vim-toml', { branch = 'main' })
-  Plug ('peitalin/vim-jsx-typescript')
-  Plug ('jelera/vim-javascript-syntax')
-  Plug ('tpope/vim-markdown')
 
   Plug ('iamcco/markdown-preview.nvim', {
     ['do'] = function() vim.fn["mkdp#util#install"]() end,
