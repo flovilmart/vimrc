06cd8ef21cde60388c9d21f8378d413f449dea83
Florent Vilmart <florent@flovilmart.com>
2024-10-11 16:10:08 -0400

    add nu parser to treesitter

--- plugin/after/treesitter.lua
+++ plugin/after/treesitter.lua
@@ -10,17 +10,24 @@ require'nvim-treesitter.configs'.setup {
 
     additional_vim_regex_highlighting = false,
   },
-
-  on_config_done = function()
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.ejs = {
-      install_info = {
-        url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
-        files = { "src/parser.c" },
-        requires_generate_from_grammar = true,
-      },
-      filetype = "ejs",
-    }
-
-  end,
 }
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.ejs = {
+  install_info = {
+    url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+    files = { "src/parser.c" },
+    requires_generate_from_grammar = true,
+  },
+  filetype = "ejs",
+}
+
+parser_config.nu = {
+  install_info = {
+    url = "https://github.com/nushell/tree-sitter-nu",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "nu",
+}
+vim.treesitter.language.register("html", "ejs")
