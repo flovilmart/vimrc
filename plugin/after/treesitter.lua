7a6c641659884e30b75381ce24d5be2ec8f085de
Florent Vilmart <florent@flovilmart.com>
2024-12-20 08:01:47 -0500

    Tree sitter compat w/ nushell

--- plugin/after/treesitter.lua
+++ plugin/after/treesitter.lua
@@ -15,6 +15,32 @@ require'nvim-treesitter.configs'.setup {
 
     additional_vim_regex_highlighting = false,
   },
+  -- OPTIONAL!! These enable ts-specific textobjects.
+  -- So you can hit `yaf` to copy the closest function,
+  -- `dif` to clear the content of the closest function,
+  -- or whatever keys you map to what query.
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        -- For example:
+        -- Nushell only
+        ["aP"] = "@pipeline.outer",
+        ["iP"] = "@pipeline.inner",
+
+        -- supported in other languages as well
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["aC"] = "@conditional.outer",
+        ["iC"] = "@conditional.inner",
+        ["iS"] = "@statement.inner",
+        ["aS"] = "@statement.outer",
+      }, -- keymaps
+    }, -- select
+  }, --
 }
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
@@ -30,7 +56,7 @@ parser_config.ejs = {
 parser_config.nu = {
   install_info = {
     url = "https://github.com/nushell/tree-sitter-nu",
-    files = { "src/parser.c" },
+    files = { "src/parser.c", "src/scanner.c" },
     branch = "main",
   },
   filetype = "nu",
