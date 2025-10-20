8382f851f6a2a396530e9742b100f4eb0a853a0c
Florent Vilmart <florent@flovilmart.com>
2025-10-20 09:10:49 -0400

    chore: use telescope when starting with folder

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,6 +41,15 @@ function config_telescope()
       },
     },
   }
+
+  -- Launch telescope when opening nvim with a directory
+  vim.cmd [[
+    augroup ReplaceNetrw
+        autocmd VimEnter * silent! autocmd! FileExplorer
+        autocmd StdinReadPre * let s:std_in=1
+        autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | call luaeval("require('telescope').extensions.file_browser.file_browser({cwd = _A})", argv()[0]) | endif
+    augroup END
+  ]]
 end
 
 return {
