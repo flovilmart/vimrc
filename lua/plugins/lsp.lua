04da7f4c0afde7b9d2b9671f1f74b5e0721afd03
Florent Vilmart <florent@flovilmart.com>
2025-10-22 13:38:20 -0400

    add graphQL LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,7 @@ function config()
 
   -- Use a loop to conveniently call 'setup' on multiple servers and
   -- map buffer local keybindings when the language server attaches
-  local servers = { "ts_ls", "pyright", "bashls", "nushell" }
+  local servers = { "ts_ls", "pyright", "bashls", "nushell", "graphql" }
   local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   for _, lsp in ipairs(servers) do
